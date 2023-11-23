c804bca30b11b3d103442f79fddceef773053efa
OsamaIMR <133792269+OsamaIMR@users.noreply.github.com>
2023-11-22 21:18:52 -0600

    Create Main.kt

---
+++ Viaje Nacional/Main.kt
@@ -0,0 +1,7 @@
+import Clases.*
+fun  main(){
+    val travel = Travel
+    val viajeMonterrey = National("Monterrey")
+    viajeMonterrey.quotePrice(2)
+    val viajeBajaMonterrey = NationalLowSeason("Monterrey")
+}
